116e3094220268c9edcd0ab9afa0bdc2ef59cedc
Your-Packing-Friend <anuzuga123@gmail.com>
2025-10-08 12:41:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,8 @@
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('zuga-cache').then(cache => {
-      return cache.addAll(['./index.html', './manifest.json']);
-    })
-  );
+  e.waitUntil(caches.open('zuga-cache').then(cache => cache.addAll([
+    './', './index.html', './manifest.json'
+  ])));
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
